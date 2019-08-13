c5f469e2ee8b48bc08e75715e67fae6ed2b835e6
양혜령 <hyeryung.yang@bankwareglobal.com>
2019-08-13 11:03:02 +0900

    delete vector-icons modules

--- android/app/src/main/java/com/morningmeongmeong/MainApplication.java
+++ android/app/src/main/java/com/morningmeongmeong/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new AsyncStoragePackage(),
-            new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
     }
