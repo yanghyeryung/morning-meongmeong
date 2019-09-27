d085c7e2c0ed5e043f19bab5daedba65bdfda164
양혜령 <hyeryung.yang@bankwareglobal.com>
2019-09-27 15:50:23 +0900

    add background job module

--- App.js
+++ App.js
@@ -6,6 +6,22 @@ import ListScreen from "./components/ListScreen";
 import AddEditScreen from "./components/AddEditScreen";
 import AlarmScreen from "./components/AlarmScreen";
 
+import BackgroundJob from 'react-native-background-job';
+
+const backgroundJob = {
+    jobKey: "myJob",
+    job: () => console.log("Running in background")
+};
+
+BackgroundJob.register(backgroundJob);
+
+BackgroundJob.schedule({
+    jobKey: "myJob",
+    period: 1000,
+    exact: true,
+    allowExecutionInForeground: true
+});
+
 let headerOptions = {
     headerTitle: <Header />,
     headerLeft: null
